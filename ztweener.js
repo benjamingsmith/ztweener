d99a04a801f3a24912ebeeb94776d2aae9ceb8e9
Benjamin Smith <benjamin.smith@72andsunny.com>
2015-09-02 11:05:59 -0700

    v1

---
+++ ztweener.js
@@ -0,0 +1,78 @@
+function($) {
+  function filter(obj1, obj2) {
+    var result = {};
+    for (key in obj1) {
+      if (obj2[key] != obj1[key]) result[key] = obj2[key];
+      if (typeof obj2[key] == 'array' && typeof obj1[key] == 'array')
+        result[key] = arguments.callee(obj1[key], obj2[key]);
+      if (typeof obj2[key] == 'object' && typeof obj1[key] == 'object')
+        result[key] = arguments.callee(obj1[key], obj2[key]);
+    }
+    return result;
+  }
+
+  $.fn.getStyleObject = function() {
+    var dom = this.get(0);
+    var style;
+    var returns = {};
+    if (window.getComputedStyle) {
+      var camelize = function(a, b) {
+        return b.toUpperCase();
+      };
+      style = window.getComputedStyle(dom, null);
+      for (var i = 0, l = style.length; i < l; i++) {
+        var prop = style[i];
+        var camel = prop.replace(/\-([a-z])/g, camelize);
+        var val = style.getPropertyValue(prop);
+        returns[camel] = val;
+      };
+      return returns;
+    };
+    if (style = dom.currentStyle) {
+      for (var prop in style) {
+        returns[prop] = style[prop];
+      };
+      return returns;
+    };
+    return this.css();
+  }
+
+  $.delayedCall = function(delay, funct) {
+    setTimeout(function() {
+      funct();
+    }, delay * 1000);
+  }
+
+  $.fn.animateFrom = function(fromProperties, duration, easing, callback, delay) {
+    // get inital styles from element
+    var originalProperties = $(this).getStyleObject();
+    $(this).css(
+      fromProperties
+    );
+    // check what values have changed
+    var oldValues = filter($(this).getStyleObject(), originalProperties);
+
+    $(this).animate(oldValues, duration * 1000, easing, function() {
+      if (callback) {
+        callback();
+      } else {
+        callback
+      }
+    }, delay * 1000);
+  }
+
+  $.fn.animateTo = function(toProperties, duration, easing, callback, delay) {
+    var that = $(this);
+    setTimeout(function() {
+      $(that).animate(
+        toProperties, duration * 1000, easing,
+        function() {
+          if (callback) {
+            callback();
+          } else {
+            callback
+          }
+        }, 0);
+    }, delay * 1000);
+  }
+})(Zepto);
